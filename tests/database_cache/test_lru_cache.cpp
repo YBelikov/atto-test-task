cff655c408de35c55c0099304468c27d38b5e365
Yuriy Belikov <yuriybelikov1@gmail.com>
2025-03-02 22:38:03 +0200

    Add more tests

--- tests/database_cache/test_lru_cache.cpp
+++ tests/database_cache/test_lru_cache.cpp
@@ -99,3 +99,100 @@ TEST_F(DatabaseCacheTest, RemoveCommit) {
 
     EXPECT_EQ(db.get("RemKey"), "");  // Should be deleted after commit
 }
+
+TEST_F(DatabaseCacheTest, CacheEviction) {
+    db.begin_transaction();
+    db.set("1", "One");
+    db.set("2", "Two");
+    db.set("3", "Three");
+    db.set("4", "Four");
+    db.set("5", "Five");
+    db.set("6", "Six");  // This should evict the least recently used key
+    db.commit_transaction();
+
+    EXPECT_EQ(db.get("1"), "One");  // "1" should be evicted
+    EXPECT_EQ(db.get("6"), "Six");  // "6" should be in cache
+}
+
+TEST_F(DatabaseCacheTest, MultipleSetSameKey) {
+    db.begin_transaction();
+    db.set("A", "Value1");
+    db.set("A", "Value2");
+    db.set("A", "Value3");
+    EXPECT_EQ(db.get("A"), "Value3");  // Should reflect the last write
+    db.commit_transaction();
+
+    EXPECT_EQ(db.get("A"), "Value3");  // Ensure final value persists
+}
+
+TEST_F(DatabaseCacheTest, MultipleRemoveSameKey) {
+    db.begin_transaction();
+    db.set("B", "ToBeDeleted");
+    db.commit_transaction();
+    
+    db.begin_transaction();
+    db.remove("B");
+    db.remove("B");
+    EXPECT_EQ(db.get("B"), "");  // Should be deleted
+    db.commit_transaction();
+
+    EXPECT_EQ(db.get("B"), "");  // Should remain deleted
+}
+
+TEST_F(DatabaseCacheTest, RemoveBeforeSet) {
+    db.begin_transaction();
+    db.remove("C");  // Remove before setting
+    db.set("C", "NewValue");
+    EXPECT_EQ(db.get("C"), "NewValue");  // Should return the newly set value
+    db.commit_transaction();
+
+    EXPECT_EQ(db.get("C"), "NewValue");  // Should persist
+}
+
+TEST_F(DatabaseCacheTest, SetRemoveSameKeyInTransaction) {
+    db.begin_transaction();
+    db.set("D", "IntermediateValue");
+    EXPECT_EQ(db.get("D"), "IntermediateValue");
+
+    db.remove("D");
+    EXPECT_EQ(db.get("D"), "");  // Should be removed
+
+    db.set("D", "FinalValue");
+    EXPECT_EQ(db.get("D"), "FinalValue");  // Should return last written value
+    db.commit_transaction();
+
+    EXPECT_EQ(db.get("D"), "FinalValue");  // Ensure final value persists
+}
+
+TEST_F(DatabaseCacheTest, RollbackMultipleChanges) {
+    db.begin_transaction();
+    db.set("E", "ValueE");
+    db.set("F", "ValueF");
+    db.remove("G");
+    db.remove("H");
+
+    EXPECT_EQ(db.get("E"), "ValueE");
+    EXPECT_EQ(db.get("F"), "ValueF");
+    EXPECT_EQ(db.get("G"), "");
+    EXPECT_EQ(db.get("H"), "");
+
+    db.abort_transaction();
+
+    EXPECT_EQ(db.get("E"), "");  // Should NOT exist
+    EXPECT_EQ(db.get("F"), "");  // Should NOT exist
+    EXPECT_EQ(db.get("G"), "");  // Should NOT exist
+    EXPECT_EQ(db.get("H"), "");  // Should NOT exist
+}
+
+TEST_F(DatabaseCacheTest, ConsecutiveTransactionsModifySameKey) {
+    db.begin_transaction();
+    db.set("Z", "Transaction1");
+    db.commit_transaction();
+
+    db.begin_transaction();
+    EXPECT_EQ(db.get("Z"), "Transaction1");  // Should see previous transaction's value
+    db.set("Z", "Transaction2");
+    db.commit_transaction();
+
+    EXPECT_EQ(db.get("Z"), "Transaction2");  // Should persist the latest update
+}
